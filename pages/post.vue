1b15fe91b2938715a368f4534c88ad737229083f
matt_thetechmillion <matt@thetechmillion.com>
2020-01-13 23:24:31 +0200

    Added inline ckeditor | started on post add

--- pages/post.vue
+++ pages/post.vue
@@ -11,14 +11,59 @@
           <a-step title="The Role" />
           <a-step title="Responsibilities" />
           <a-step title="Salary" />
-          <a-step title="Extras" />
+          <a-step title="Finalise" />
         </a-steps>
       </p>
+      <div class="card">
+        <div class="section">
+          <h4 class="mb-0">
+            A quick intro of your company
+            <a-badge
+              count="required"
+              :number-style="{ backgroundColor: '#52c41a' }"
+            />
+          </h4>
+          <p>Some text here (keep it short)</p>
+          <ckeditor :editor="editor"></ckeditor>
+        </div>
+        <div class="section">
+          <h4>Website URL</h4>
+          <a-input default-value="mysite">
+            <a-select slot="addonBefore" default-value="http://">
+              <a-select-option value="http://">http://</a-select-option>
+              <a-select-option value="https://">https://</a-select-option>
+            </a-select>
+          </a-input>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+<script>
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import ClassicEditor from '@ckeditor/ckeditor5-build-inline'
+
+export default {
+  components: {
+    ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor
+
+      // ...
+    }
+  }
+}
+</script>
 <style scoped>
 .container {
   max-width: 1500px;
+}
+.card {
+  padding: 15px;
+}
+.section {
+  margin-bottom: 15px;
 }
 </style>
