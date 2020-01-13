dec64f828350dac24e5ea5105c388fabf6fe0948
matt_thetechmillion <matt@thetechmillion.com>
2020-01-13 17:58:41 +0200

    Added steps to post page

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
         <a-button type="danger">Post a job</a-button>
       </nuxt-link>
     </div>
-    <h1 class="title">Find tech jobs around the world</h1>
+    <h1 class="title heading heading--x-large">
+      Find tech jobs around the world
+    </h1>
   </div>
 </template>
--- pages/post.vue
+++ pages/post.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1 class="title heading heading--x-large">
+    <h1 class="title heading heading--x-large pb-0">
       Make your search available to the tech world
     </h1>
+    <p class="align-center sub-title">Add some text here</p>
+    <p>
+      <a-steps :current="1">
+        <a-step title="Your Company" />
+        <a-step title="The Role" />
+        <a-step title="Responsibilities" />
+        <a-step title="Salary" />
+        <a-step title="Extras" />
+      </a-steps>
+    </p>
   </div>
 </template>
