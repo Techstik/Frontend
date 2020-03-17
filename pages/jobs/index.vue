248f35187f2bd9707e643a48462e5cc088636973
matt_thetechmillion <matt@thetechmillion.com>
2020-03-17 22:02:00 +0200

    Started writing the search functionality

--- pages/jobs/index.vue
+++ pages/jobs/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <listing v-for="post in posts" :key="post.id" :post="post" />
+    <div class="search-bar">
+      <a-input v-model="searchWord" />
+      <p class="filter-text">Apply some filters</p>
+    </div>
+    <h3 class="subheading">Posted Today</h3>
+    <listing v-for="post in searchFilter" :key="post.id" :post="post" />
   </div>
 </template>
 
@@ -13,13 +18,43 @@ export default {
   },
   data() {
     return {
-      posts: []
+      posts: [],
+      searchWord: ''
     }
   },
-  mounted() {
+  computed: {
+    searchFilter() {
+      if (!this.searchWord) return this.posts
+
+      return this.posts.filter(post => {
+        return (
+          post.position.toLowerCase().includes(this.searchWord.toLowerCase()) ||
+          post.company_name
+            .toLowerCase()
+            .includes(this.searchWord.toLowerCase())
+        )
+      })
+    }
+  },
+  created() {
     this.$readData('posts', { limit: 3 }).then(data => {
       this.posts = data
     })
   }
 }
 </script>
+<style scoped>
+.subheading {
+  color: white;
+}
+.search-bar {
+  background-color: transparent;
+  padding: 30px 50px;
+  border-radius: 6px;
+}
+.search-bar .filter-text {
+  font-family: Graphik;
+  text-align: center;
+  color: white;
+}
+</style>
