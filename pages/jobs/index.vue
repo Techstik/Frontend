92fa106f7db81f1e9fb38cd9bf66980f2c6dc3e5
matt_thetechmillion <matt@thetechmillion.com>
2020-03-17 14:30:46 +0200

    Added new jobs page

---
+++ pages/jobs/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <listing />
+</template>
+
+<script>
+import listing from '@/components/listings/listing'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    listing
+  },
+  computed: {
+    ...mapState({})
+  }
+}
+</script>
