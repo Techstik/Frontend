606a8ff5b0c6bdbb7c9cade749e61d5c25832e1e
matt_thetechmillion <matt@thetechmillion.com>
2020-04-15 19:14:37 +0200

    Renamed Injections to injections

--- plugins/Injections/bugsnag.js
+++ plugins/Injections/bugsnag.js
@@ -5,6 +5,7 @@ import bugsnagVue from '@bugsnag/plugin-vue'
 const bugsnagClient = bugsnag(process.env.BUGSNAG_KEY)
 bugsnagClient.use(bugsnagVue, Vue)
 
-export default inject => {
+// eslint-disable-next-line no-unused-vars
+export default ({ app }, inject) => {
   inject('bugsnag', bugsnagClient)
 }
