448a8b9574f5231722231c0d0001baf3a9cefa1c
matt_thetechmillion <matt@thetechmillion.com>
2020-04-22 18:26:59 +0200

    Added tweet functionality to functions

---
+++ functions/twitter.js
@@ -0,0 +1,20 @@
+const firebase_functions = require('firebase-functions')
+const Twitter = require('twitter-lite')
+
+const client = new Twitter({
+  consumer_key: firebase_functions.config().twitter.consumer_key,
+  consumer_secret: firebase_functions.config().twitter.consumer_secret,
+  access_token_key: firebase_functions.config().twitter.access_token,
+  access_token_secret: firebase_functions.config().twitter.access_token_secret
+})
+
+function tweet(thread) {
+  return new Promise((resolve, reject) => {
+    client
+      .post('statuses/update', {
+        status: status
+      })
+      .then(() => resolve())
+      .catch(error => reject(error))
+  })
+}
